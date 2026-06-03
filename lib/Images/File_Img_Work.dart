125c155da110800f5e4bc22ee20fb2dd52c28e2d
muhammad-huzaifa-ali <muhammadhuzali224@gmail.com>
2026-06-03 17:39:02 +0500

    Image Work

--- lib/Images/File_Img_Work.dart
+++ lib/Images/File_Img_Work.dart
@@ -1,57 +1,108 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class FileImgWork extends StatefulWidget {
-  const FileImgWork({super.key});
+class ImagePickerScreen extends StatefulWidget {
+  const ImagePickerScreen({super.key});
 
   @override
-  State<FileImgWork> createState() => _FileImgWorkState();
+  State<ImagePickerScreen> createState() => _ImagePickerScreenState();
 }
 
-class _FileImgWorkState extends State<FileImgWork> {
-  File? image;
+class _ImagePickerScreenState extends State<ImagePickerScreen> {
 
-  // 📌 REAL FUNCTION
-  Future<void> pickImage() async {
-    final picker = ImagePicker();
+  File? imageFile;
+  final ImagePicker picker = ImagePicker();
 
-    final pickedFile =
-    await picker.pickImage(source: ImageSource.gallery);
+  Future<void> requestPermission() async {
+    await Permission.camera.request();
+    await Permission.photos.request();
+  }
 
-    if (pickedFile != null) {
+  Future<void> pickImage(ImageSource source) async {
+    await requestPermission();
+
+    final XFile ? pickedImage = await picker.pickImage(
+      source: source,
+      imageQuality: 70,
+    );
+
+    if(pickedImage != null){
       setState(() {
-        image = File(pickedFile.path); // 🔥 Image.file yahan se connect hota hai
+        imageFile = File(pickedImage.path);
       });
     }
+  }
+
+  void showImagePickerOptions() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return SafeArea(
+            child: Wrap(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.camera_alt),
+                  title: Text('Take a Photo'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    pickImage(ImageSource.camera);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.photo_library),
+                  title: Text('Choose from Gallery'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    pickImage(ImageSource.gallery);
+                  },
+                ),
+              ],
+            ),
+          );
+        }
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Real Image.file Example")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        title: Text('Image Picker',style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.green,
+      ),
+      body:  Center(
+        child: Stack(
           children: [
-
-            // 📌 SHOW IMAGE
-            image != null
-                ? Image.file(
-              image!,
-              width: 200,
-              height: 200,
-              fit: BoxFit.cover,
-            )
-                : const Text("No Image Selected"),
-
-            const SizedBox(height: 20),
-
-            // 📌 BUTTON
-            ElevatedButton(
-              onPressed: pickImage,
-              child: const Text("Pick Image"),
+            CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.grey.shade300,
+              backgroundImage: imageFile != null ? FileImage(imageFile!): null,
+              child: imageFile == null ?
+              Icon(Icons.person,size: 70,color: Colors.white,) : null,
             ),
+
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: GestureDetector(
+                onTap: showImagePickerOptions,
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      )
+                  ),
+                  child: Icon(Icons.edit,size: 20,color: Colors.white,),
+                ),
+              ),
+            )
           ],
         ),
       ),
